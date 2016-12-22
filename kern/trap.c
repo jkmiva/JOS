7befd05e7e2ff2d1972d5853693cfadc844a46d5
jkmiva <10102140241@stu.ecnu.edu.cn>
2016-12-22 15:15:46 -0600

    resolve merge conflict

--- kern/trap.c
+++ kern/trap.c
@@ -261,14 +261,6 @@ trap_dispatch(struct Trapframe *tf)
 	// Handle keyboard and serial interrupts.
 	// LAB 5: Your code here.
 
-	// Unexpected trap: The user process or the kernel has a bug.
-	print_trapframe(tf);
-	if (tf->tf_cs == GD_KT)
-		panic("unhandled trap in kernel");
-	else {
-		env_destroy(curenv);
-		return;
-
 
 	switch (tf->tf_trapno) {
 		case T_PGFLT: {
